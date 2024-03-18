83a35f90e414036d82e4d1ceacec55a23e872025
Matt Lee <mattlee0717@gmail.com>
2024-03-18 13:42:16 -0700

    Fixed signup.js code after testing merge conflicts

--- scripts/signup.js
+++ scripts/signup.js
@@ -17,7 +17,7 @@ document.getElementById('signup-form').addEventListener('submit', function (even
 
       // Add user info to Firestore
       db.collection("users").doc(user.uid).set({
-        name: na,
+        name: name,
         email: email,
         // Additional default information
         country: "Canada",
