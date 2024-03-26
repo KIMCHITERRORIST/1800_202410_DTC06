cef52bb23b9fd2ce085ea0909c048329d4ddfaec
Nu Ei Wai <nueiwai724665@gmail.com>
2024-03-26 03:59:13 -0700

    Navigation for general pages

--- scripts/general_nav_bottom.js
+++ scripts/general_nav_bottom.js
@@ -4,13 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const settingsBtn = document.getElementById("settings-btn");
   const plusBtn = document.getElementById("plus-btn");
   const userBtn = document.getElementById("user-btn");
+  // Variables for Event listener for Plus button  
+  const menu = document.getElementById("menu");
+  const menuBg = document.getElementById("menuBackground");
+  const closeBtn = document.getElementById("closeMenu");
 
   // Event listener for Home button
-  homeBtn.addEventListener("click", () => {
-    console.log("Redirecting to Home Page...");
-    // Code to redirect to Home Page
-    window.location.href = "overview.html";
-  });
+  if (homeBtn) {
+    homeBtn.addEventListener("click", () => {
+      console.log("Redirecting to Home Page...");
+      // Code to redirect to Home Page
+      window.location.href = "../overview.html";
+    });
+  } else { console.log("Home button not found"); }
 
   // Event listener for Settings button
   settingsBtn.addEventListener("click", () => {
@@ -19,13 +25,27 @@ document.addEventListener("DOMContentLoaded", () => {
     // Example: openSettingsFunction();
   });
 
-  // Event listener for Plus button
+  // Open menu event
   plusBtn.addEventListener("click", () => {
-    console.log("Displaying Popup Menu...");
+    menu.classList.remove("hidden");
+    menuBg.classList.remove("hidden");
+  });
+
+  // Close menu event
+  closeBtn.addEventListener("click", () => {
+    menu.classList.add("hidden");
+    menuBg.classList.add("hidden");
+  });
+
+  // Close menu on outside click
+  menuBg.addEventListener("click", () => {
+    menu.classList.add("hidden");
+    menuBg.classList.add("hidden");
   });
 
   // Event listener for User button
   userBtn.addEventListener("click", () => {
-    console.log("Redirecting to User Settings...");
+    console.log("Redirecting to User Profile...");
+    window.location.href = "../profile.html";
   });
 });
