d794926c16bb7cc8e1db85b6b64f47d852a54ff2
Nu Ei Wai <nueiwai724665@gmail.com>
2024-03-31 00:09:32 -0700

    Change the card layouts using icons

--- scripts/exercise_log.js
+++ scripts/exercise_log.js
@@ -24,29 +24,39 @@ async function fetchAndDisplayUserActivities() {
       // Generate HTML for each activity
       exerciseCardContainer.innerHTML += `
         <div class="bg-white p-4 rounded-lg shadow-lg w-full">
-              <div class="flex items-center space-x-10">
-                <div class="flex-shrink-0">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler   icon-tabler-barbell" width="40" height="40"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="#172d58"   fill="none" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M2 12h1"/>
-                    <path d="M6 8h-2a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h2"/>
-                    <path d="M6 7v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-10a1 1 0 0 0   -1 -1h-1a1 1 0 0 0 -1 1z"/>
-                    <path d="M9 12h6"/>
-                    <path d="M15 7v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-10a1 1 0 0  0 -1 -1h-1a1 1 0 0 0 -1 1z"/>
-                    <path d="M18 8h2a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-2"/>
-                    <path d="M22 12h-1"/>
-                  </svg>
+              <div class="flex items-center">
+                <div class="flex container space-x-10">
+                  <div class="flex-shrink-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler   icon-tabler-barbell" width="40" height="40"
+                      viewBox="0 0 24 24" stroke-width="1.5" stroke="#172d58"   fill="none" stroke-linecap="round"
+                      stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M2 12h1"/>
+                      <path d="M6 8h-2a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h2"/>
+                      <path d="M6 7v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-10a1 1 0 0 0   -1 -1h-1a1 1 0 0 0 -1 1z"/>
+                      <path d="M9 12h6"/>
+                      <path d="M15 7v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-10a1 1 0 0  0 -1 -1h-1a1 1 0 0 0 -1 1z"/>
+                      <path d="M18 8h2a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-2"/>
+                      <path d="M22 12h-1"/>
+                    </svg>
+                  </div>
+                  <div>
+                    <div class="text-xl text-black font-medium">Activity Name:<span class= "font-normal"> ${name}</span></div>
+                    <p class="text-black font-medium">Date: <span class="font-normal">${date}</span></p>
+                    <p class="text-black font-medium">Time: <span class="font-normal">${time}</span></p>
+                    <p class="text-black font-medium">Duration: <span class="font-normal">${hour}h ${minute}m ${second}s</span></p>
+                  </div>
                 </div>
-                <div>
-                  <div class="text-xl text-black">Activity Name: <span  class="font-medium">${name}</span></div>
-                  <p class="text-black">Date: ${date}</p>
-                  <p class="text-black">Time: ${time}</p>
-                  <p class="text-black">Duration: ${hour}h ${minute}m ${second}s</p>
-                  <p class="text-black">Calories Burnt: ${caloriesBurned}</p>
+                <div class="flex flex-col text-center items-center justify-end">
+                  <p class="text-black text-lg font-bold">Burnt</p>
+                  <div>
+                    <p class="text-black text-lg font-medium"><span class="font-normal">${caloriesBurned}</span></p>
+                    <img src="/images/kcal_icon.svg" alt="Calories Icon" class="w-10 h-10">
+                  </div>
+                </div>
               </div>
-            </div>`;
+            </div>
+      </div>`;
     });
   } catch (error) {
     console.error("Error fetching user activities:", error);
