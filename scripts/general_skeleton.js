f3c2adb993e9368488e686293a034acb8a327761
Nu Ei Wai <nueiwai724665@gmail.com>
2024-04-05 08:19:39 -0700

    Add recipe modal to bottom navbar

--- scripts/general_skeleton.js
+++ scripts/general_skeleton.js
@@ -71,11 +71,16 @@ function loadSkeleton() {
     });
 
     document.getElementById('open_add_ingredient_modal').addEventListener('click', loadIngredientsModalandOpen);
+    document.getElementById('open_add_new_recipe_modal').addEventListener('click', loadAddNewRecipeModalandOpen);
   })
+
 };
 
 loadSkeleton();
 
+//------------------------------------------------------------------//
+// -----------Functions related to Add Ingredient Modal-------------//
+//------------------------------------------------------------------//
 function loadIngredientsModalandOpen() {
   $('#add_ingredient_modal_container').load('main_modals/add_ingredients_modal.html', function () {
     const quickAddMenu = document.getElementById("quickAddMenu");
@@ -110,15 +115,166 @@ function closeAddIngredientModal() {
   modal.classList.add('hidden');
   modal.setAttribute('aria-hidden', 'true');
 }
+
+//--------------------------------------------------------------//
+// -----------Functions related to Add Recipe Modal-------------//
+//--------------------------------------------------------------//
+
+function loadAddNewRecipeModalandOpen() {
+  $('#add_new_recipe_modal_container').load('main_modals/add_new_recipe_modal.html', function () {
+    const quickAddMenu = document.getElementById("quickAddMenu");
+    const quickAddMenuBg = document.getElementById("quickAddMenuBackground");
+    quickAddMenu.classList.add("hidden");
+    quickAddMenuBg.classList.add("hidden");
+    openAddNewRecipeModal(); // Open Add New Recipe modal
+
+    let ingredientIndex = 0;
+    // Append categories to dropdown in modal
+    appendCategoriesToDropdownInModal();
+
+    // Event listeners for Add Ingredient modal
+    document.getElementById('close_add_new_recipe_modal').addEventListener('click',
+      closeAddNewRecipeModal);
+
+    const addNewRecipeButton = document.getElementById('addNewRecipeButton');
+    addNewRecipeButton.addEventListener('click', function (saveData) {
+      saveData.preventDefault(); // Prevent the default form submission
+      saveRecipeInDB();
+    });
+
+    const addIngredientFieldButton = document.getElementById('add-ingredient-field');
+    addIngredientFieldButton.addEventListener('click', () => {
+      addIngredientField(ingredientIndex);
+      ingredientIndex++;
+    })
+  }
+  )
+};
+
+// Open Add Recipe modal
+function openAddNewRecipeModal() {
+  const modal = document.getElementById('add_new_recipe_modal');
+  modal.classList.remove('hidden');
+  modal.setAttribute('aria-hidden', 'false');
+}
+
+// Close Add Recipe modal
+function closeAddNewRecipeModal() {
+  const modal = document.getElementById('add_new_recipe_modal');
+  modal.classList.add('hidden');
+  modal.setAttribute('aria-hidden', 'true');
+}
+
+
+function addIngredientField(ingredientIndex) {
+  let ingredientFieldDiv = `
+    <div class="container gap-4 flex flex-row justify-between items-center">
+      <div class="flex container">
+        <select id="ingredientForNewRecipes${ingredientIndex}" name="ingredientForNewRecipes${ingredientIndex}" required
+          class="mt-1 block w-full p-2.5 rounded-md border-gray-900 bg-gray-50 shadow-sm focus:border-[#172d58] focus:ring-[#172d58] sm:text-sm">
+          <option value="" disabled selected>ingredient</option>
+          <!-- Your options here -->
+        </select>
+      </div>
+      <div class="flex container">
+        <input type="number" name="quantity${ingredientIndex}" id="quantity${ingredientIndex}"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+          placeholder="quantity" required>
+      </div>
+      <div class="flex container">
+        <input type="text" name="unit${ingredientIndex}" id="unit${ingredientIndex}"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+          placeholder="unit(auto)" required>
+      </div>
+    </div>`;
+
+  document.getElementById('ingredientsAdditionForNewRecipe').insertAdjacentHTML('beforeend', ingredientFieldDiv);
+  // Append ingredients and associated unit to dropdowns in modal
+  appendIngredientsToDropdownInModal(ingredientIndex);
+  changeUnitAfterIngredientSelected(ingredientIndex);
+  ingredientIndex++;
+}
+
+function changeUnitAfterIngredientSelected(ingredientIndex) {
+  document.getElementById(`ingredientForNewRecipes${ingredientIndex}`).addEventListener('change', async () => {
+    const unit = await fetchUnitForIngredient(ingredientIndex);
+    document.getElementById(`unit${ingredientIndex}`).value = unit;
+  });
+}
+
+// ====== For saving data in DB for new recipe (calculating nutritional values)====== //
+async function getBaseValues(ingredientName) {
+  const uid = await fetchUID();
+  const ingredientRef = db.collection('ingredients').doc(uid);
+  const allIngredientSnapshot = await ingredientRef.get();
+  const ingredientSnapshot = allIngredientSnapshot.data()[ingredientName];
+  console.log(ingredientSnapshot);
+  return ingredientSnapshot;
+}
+
+function calculateNutrition(baseValues, inputQuantity) {
+  const ratio = inputQuantity / baseValues.quantity.value;
+  return {
+    calories: Math.round(baseValues.calories * ratio),
+    carbs: Math.round(baseValues.carbs * ratio),
+    fat: Math.round(baseValues.fat * ratio),
+    protein: Math.round(baseValues.protein * ratio)
+  };
+}
+
+function collectRecipeDataFromForm() {
+  const ingredientsDataForAddingToRecipe = {};
+  let ingredientIndex = 0;
+  while (true) {
+    const ingredient = document.getElementById(`ingredientForNewRecipes${ingredientIndex}`);
+    const quantity = document.getElementById(`quantity${ingredientIndex}`);
+    const unit = document.getElementById(`unit${ingredientIndex}`);
+
+    if (!ingredient || !quantity || !unit) {
+      // Exit the loop if any of the fields are missing
+      break;
+    }
+
+    // Extract values from form fields
+    const ingredientName = ingredient.value;
+    const quantityValue = quantity.value;
+    const unitValue = unit.value;
+
+    ingredientsDataForAddingToRecipe[ingredientName] = {
+      quantity: {
+        unit: unitValue,
+        value: Number(quantityValue)
+      }
+    };
+
+    ingredientIndex++;
+  }
+  return ingredientsDataForAddingToRecipe;
+}
+//---------------------------------------------------//
 //---------------------------------------------------//
 // -----------Functions related to DB----------------//
 //---------------------------------------------------//
+//---------------------------------------------------//
+// Fetch UID function
+async function fetchUID() {
+  return new Promise((resolve, reject) => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        resolve(user.uid);
+      } else {
+        reject('User is not logged in.');
+      }
+    });
+  });
+}
 
+// ++++++ For add Ingredient modal ++++++ //
 async function saveIngredientInDB() {
   const uid = await fetchUID();
   console.log(uid);
   const ingredientName = document.getElementById('ingredientName').value.trim();
-  const fats = Number(document.getElementById('fats').value);
+  const fat = Number(document.getElementById('fats').value);
   const carbs = Number(document.getElementById('carbs').value);
   const protein = Number(document.getElementById('protein').value);
   const calories = Number(document.getElementById('calories').value);
@@ -128,7 +284,7 @@ async function saveIngredientInDB() {
   // Format data to update in Firestore
   let dataToUpdate = {
     [ingredientName]: {
-      fats,
+      fat,
       carbs,
       protein,
       calories,
@@ -148,4 +304,111 @@ async function saveIngredientInDB() {
     .catch((error) => {
       console.error('Error updating document:', error);
     });
+}
+
+// ++++++ For add new recipe modal ++++++ //
+async function fetchCategoriesForNewRecipes() {
+  const uid = await fetchUID();
+  console.log(uid);
+  doc = await db.collection("Recipes").doc(uid).get();
+  if (doc.exists) {
+    console.log("Document data:", doc.data());
+    return doc.data().categories;
+  }
+  else {
+    console.log("No such document!");
+  }
+}
+
+async function appendCategoriesToDropdownInModal() {
+  const categories = await fetchCategoriesForNewRecipes();
+  const selectElement = document.getElementById("categoriesForNewRecipes");
+
+  categories.forEach(category => {  // Iterate through each category and append it as an option
+    const option = document.createElement('option');
+    option.value = category;
+    option.textContent = category;
+    selectElement.appendChild(option);
+  });
+}
+
+async function fetchIngredientsForNewRecipes() {
+  const uid = await fetchUID();
+  console.log(uid);
+  doc = await db.collection("ingredients").doc(uid).get();
+  if (doc.exists) {
+    console.log("Document data:", doc.data());
+    return Object.keys(doc.data());
+  }
+  else {
+    console.log("No such document!");
+  }
+}
+
+async function appendIngredientsToDropdownInModal(ingredientIndex) {
+  const ingredients = await fetchIngredientsForNewRecipes();
+  const selectElement = document.getElementById(`ingredientForNewRecipes${ingredientIndex}`);
+
+  ingredients.forEach(ingredient => {  // Iterate through each ingredient and append it as an option
+    const option = document.createElement('option');
+    option.value = ingredient;
+    option.textContent = ingredient;
+    selectElement.appendChild(option);
+  });
+}
+
+async function fetchUnitForIngredient(ingredientIndex) {
+  const uid = await fetchUID();
+  console.log("ingredientIndex in fetch unit function:", ingredientIndex)
+  const ingredientsDocRef = db.collection('ingredients').doc(uid);
+  const ingredientDocSnapshot = await ingredientsDocRef.get();
+  const ingredientNameElement = document.getElementById(`ingredientForNewRecipes${ingredientIndex}`);
+  const ingredientName = ingredientNameElement.value;
+  const ingredientData = ingredientDocSnapshot.data();
+  const ingredientUnit = ingredientData[ingredientName].quantity.unit;
+  console.log(ingredientUnit);
+  return ingredientUnit;
+}
+
+// ++++++ Function to save data in DB for new recipe ++++++ //
+async function saveRecipeInDB() {
+  try {
+    const uid = await fetchUID();
+    const category = document.getElementById('categoriesForNewRecipes').value;
+    const recipeName = document.getElementById('recipeName').value.trim();
+    const recipeRef = db.collection('Recipes').doc(uid).collection(category).doc(recipeName);
+    const ingredientsData = collectRecipeDataFromForm();
+
+    let totalFat = 0,
+      totalCarbs = 0,
+      totalCalories = 0,
+      totalProtein = 0;
+
+    for (var ingredientName in ingredientsData) {
+      const baseValues = await getBaseValues(ingredientName);
+      console.log(baseValues, ingredientsData[ingredientName].quantity.value);
+      const nutrition = calculateNutrition(baseValues, ingredientsData[ingredientName].quantity.value);
+      ingredientsData[ingredientName] = nutrition;
+
+      totalFat += nutrition.fat;
+      totalCarbs += nutrition.carbs;
+      totalCalories += nutrition.calories;
+      totalProtein += nutrition.protein;
+    }
+
+    const recipeNutritionTotals = {
+      fats: totalFat,
+      carbs: totalCarbs,
+      calories: totalCalories,
+      protein: totalProtein,
+      ingredients: ingredientsData
+    };
+
+    await recipeRef.set(recipeNutritionTotals);
+    localStorage.setItem('selectedCategory', category);
+    window.location.href = '/each_category.html'; // Redirect after successful update
+  } catch (error) {
+    console.error('Error saving recipe:', error);
+    alert('Error saving the recipe') // Log any errors that occur
+  }
 }
