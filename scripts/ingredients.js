bf0c7301137f62b00e4cc5bc782a74268d71fac2
Nu Ei Wai <nueiwai724665@gmail.com>
2024-04-07 20:29:48 -0700

    Edit modal added to ingredients page

--- scripts/ingredients.js
+++ scripts/ingredients.js
@@ -4,6 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
             // User is signed in, fetch user ID and run other functions
             var uid = user.uid;
             fetchAndDisplayIngredients(uid);
+            document.getElementById('saveIngredientChanges').addEventListener('click', () => {
+                confirm("Are you sure you want to save changes?");
+                saveIngredientChanges();
+            });
+            document.getElementById('deleteIngredient').addEventListener('click', () => {
+                confirm("Are you sure you want to delete this ingredient?");
+                deleteIngredient();
+            }
+            );
         } else {
             // No user is signed in. Redirect to login page and show message
             console.log('User is not logged in. Redirecting to login page...');
@@ -21,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
 function fetchAndDisplayIngredients(uid) {
     db.collection("ingredients").doc(uid).get().then(documentSnapshot => {
         if (documentSnapshot.exists) {
@@ -33,7 +41,7 @@ function fetchAndDisplayIngredients(uid) {
                 const ingredient = ingredientNames[ingredientName];
                 // Assuming openAmountModal is a function you've defined to handle clicks.
                 var ingredientCardHTML = `
-                <div class="ingredient-card flex w-full mx-auto border-2 border-gray-300 shadow-lg rounded-lg mt-4 mb-6 p-4 items-center justify-between bg-white hover:bg-gray-50 transition-colors" data-ingredient-name="${ingredientName.toLowerCase()}" onclick="openAmountModal('${ingredientName}')">
+                <div id="${ingredientName}" class="ingredient-card flex w-full mx-auto border-2 border-gray-300 shadow-lg rounded-lg mt-4 mb-6 p-4 items-center justify-between bg-white hover:bg-gray-50 transition-colors" ondblclick="openEditIngredientModal('${ingredientName}')">
                     <div class="flex-1">
                         <p class="text-xl font-bold mb-2 text-gray-800">${ingredientName}</p>
                         <p class="text-sm text-gray-600">${ingredient.protein}g Protein | ${ingredient.carbs}g Carbs | ${ingredient.fat}g Fat | ${ingredient.calories} kcal</p>
@@ -58,5 +66,82 @@ function filterDisplayedIngredients(searchValue) {
         } else {
             card.style.display = 'none';
         }
+    });
+}
+
+async function openEditIngredientModal(ingredientName) {
+    uid = await fetchUID();
+    recipeDoc = await db.collection("ingredients").doc(uid).get()
+    recipeData = recipeDoc.data();
+
+    if (recipeData[ingredientName] === undefined) {
+        console.log("There is no such ingredient in the database.");
+    } else {
+        console.log(recipeData[ingredientName]);
+        // Set the values in the modal
+        document.getElementById('editIngredientName').value = ingredientName;
+        document.getElementById('editProtein').value = recipeData[ingredientName].protein;
+        document.getElementById('editCarbs').value = recipeData[ingredientName].carbs;
+        document.getElementById('editFat').value = recipeData[ingredientName].fat;
+        document.getElementById('editCalories').value = recipeData[ingredientName].calories;
+        document.getElementById('editQuantity').value = recipeData[ingredientName].quantity.value;
+        document.getElementById('editUnit').value = recipeData[ingredientName].quantity.unit;
+
+        // Unhide the modal
+        document.getElementById('editIngredientModal').classList.remove('hidden');
+    }
+}
+
+// Function to save changes
+async function saveIngredientChanges() {
+    event.preventDefault();
+    const uid = await fetchUID();
+    const newingredientName = document.getElementById('editIngredientName').value;
+    const newprotein = Number(document.getElementById('editProtein').value).toFixed(2);
+    const newcarbs = Number(document.getElementById('editCarbs').value).toFixed(2);
+    const newfat = Number(document.getElementById('editFat').value).toFixed(2);
+    const newcalories = Number(document.getElementById('editCalories').value).toFixed(2);
+    const newquantity = {
+        value: Number(document.getElementById('editQuantity').value).toFixed(2),
+        unit: document.getElementById('editUnit').value
+    };
+
+    console.log(newingredientName, newprotein, newcarbs, newfat, newcalories, newquantity);
+    db.collection("ingredients").doc(uid).update({
+
+        [newingredientName]: {
+            protein: newprotein,
+            carbs: newcarbs,
+            fat: newfat,
+            calories: newcalories,
+            quantity: newquantity
+        }
+    }).then(() => {
+        console.log("Document successfully updated!");
+        document.getElementById('editIngredientModal').classList.add('hidden');
+        fetchAndDisplayIngredients(uid);
+    }).catch((error) => {
+        console.error("Error updating document: ", error);
+    });
+}
+// Function to cancel changes
+function cancelIngredientChanges() {
+    window.history.back();
+}
+
+// Function to delete ingredient
+async function deleteIngredient() {
+    event.preventDefault();
+    uid = await fetchUID();
+    const ingredientName = document.getElementById('editIngredientName').value;
+
+    db.collection("ingredients").doc(uid).update({
+        [ingredientName]: firebase.firestore.FieldValue.delete()
+    }).then(() => {
+        console.log("Document successfully deleted!");
+        document.getElementById('editIngredientModal').classList.add('hidden');
+        fetchAndDisplayIngredients(uid);
+    }).catch((error) => {
+        console.error("Error deleting document: ", error);
     });
 }
